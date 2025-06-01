b982a49e54c5b738aaaa82083412cad45266c6d0
manik0120 <36017047+manik0120@users.noreply.github.com>
2025-05-31 22:31:04 -0500

    Update base.ts

--- packages/wrappers/src/base.ts
+++ packages/wrappers/src/base.ts
@@ -15,7 +15,7 @@ import {
   createLogger,
   maskSensitiveInfo,
 } from '@aiostreams/utils';
-import { fetch as uFetch, ProxyAgent } from 'undici';
+
 import { emojiToLanguage, codeToLanguage } from '@aiostreams/formatters';
 
 const logger = createLogger('wrappers');
@@ -177,7 +177,7 @@ export class BaseWrapper {
 
     let response = useProxy
       ? uFetch(url, {
-          dispatcher: new ProxyAgent(Settings.ADDON_PROXY),
+ 
           method: 'GET',
           headers: this.headers,
           signal: AbortSignal.timeout(this.indexerTimeout),
